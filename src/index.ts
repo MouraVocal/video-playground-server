815d6e81e56a52978e8d907184a14e8fb7270926
mouravocal <diego.moura3@gmail.com>
2023-10-27 14:00:23 -0300

    chore: project name

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
 import { createServer } from 'node:http'
+import { createReadStream } from 'node:fs'
 
-const port = 3000
+const port = 4000
 
 const server = createServer((req, res) => {
-  res.end('Hello World')
+  const headers = {
+    'Content-Type': 'video/mp4',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': '*'
+  }
+
+  res.writeHead(200, headers)
+
+  const videoStream = createReadStream('./video/system-of-a-down-acustico.mp4')
+
+  videoStream.pipe(res)
 }).listen(port, () => {
   console.log(`Server started on port ${port}`)
 })
